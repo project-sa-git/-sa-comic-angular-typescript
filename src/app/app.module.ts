9c71fa90a32b0cbad45f34790be449b52f4f025f
mymac <trybook1901@gmail.com>
2022-10-28 17:34:34 +0900

    manga-cardコンポーネントの追加

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MangaCardComponent } from './manga-card/manga-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MangaCardComponent
   ],
   imports: [
     BrowserModule,
