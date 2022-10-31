64f493e371eb639a619fff74c585a95902e708bb
mymac <trybook1901@gmail.com>
2022-10-31 13:37:16 +0900

    定数ファイル読み込み

--- src/app/manga-card/manga-card.component.ts
+++ src/app/manga-card/manga-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as constant from '../data/constant';
 
 @Component({
   selector: 'app-manga-card',
@@ -10,6 +11,8 @@ export class MangaCardComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    console.log(constant.COLS);
+    console.log(constant.ROWS);
   }
 
 }
