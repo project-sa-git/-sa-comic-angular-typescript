f817e8a3bb8a8bbfb9cdcfa83a247ee0364f8a23
mymac <trybook1901@gmail.com>
2022-11-06 19:05:55 +0900

    servise側でhttp（Observable形式）でバックエンドに取得し、コンポーネント側へ投げる

--- src/app/components/manga-card-detail/manga-card-detail.component.ts
+++ src/app/components/manga-card-detail/manga-card-detail.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { MangaService } from '../service/manga.service';
-import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-manga-card-detail',
@@ -15,34 +14,43 @@ export class MangaCardDetailComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.manga = this.mangaService.getManga()
 
-    // 観測対象「observable」を記述
-    const observable = new Observable((subscriber) => {
-      subscriber.next(1); // nextでデータを放出している
-      subscriber.next(2);
-      subscriber.error();
-      setTimeout(() => {  // 1s間待ってからnext4を放出
-        subscriber.next(4);
-        subscriber.complete();
-      }, 1000);
-    });
+    const mangaObservable = this.mangaService.getManga()
 
-    // debugger
+    mangaObservable.subscribe((data) => {
+      console.log("mangaObservable")
+      console.log(data)
+    })
 
-    console.log('just before subscribe');
-    observable.subscribe({
-      next(data) {
-        console.log('次のデータが出力 ' + data);
-      },
-      error(err) {
-        console.error('次のエラーが発生: ' + err);
-      },
-      complete() {
-        console.log('完了！');
-      },
-    });
-    console.log('subscribeから抜けました');
+    // this.manga = this.mangaService.getManga()
+
+    // // 観測対象「observable」を記述
+    // const observable = new Observable((subscriber) => {
+    //   subscriber.next(1); // nextでデータを放出している
+    //   subscriber.next(2);
+    //   subscriber.error();
+    //   setTimeout(() => {  // 1s間待ってからnext4を放出
+    //     subscriber.next(4);
+    //     subscriber.complete();
+    //   }, 1000);
+    // });
+
+    // // debugger
+
+    // console.log('just before subscribe');
+    // observable.subscribe({
+    //   next(data) {
+    //     console.log('次のデータが出力 ' + data);
+    //   },
+    //   error(err) {
+    //     console.error('次のエラーが発生: ' + err);
+    //   },
+    //   complete() {
+    //     console.log('完了！');
+    //   },
+    // });
+    // console.log('subscribeから抜けました');
+
   }
 
 }
--- src/app/components/service/manga.service.ts
+++ src/app/components/service/manga.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from "@angular/core";
 import * as manga from '../../../assets/data/manga';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable() // @Injectableをつけるとserviceになる
 export class MangaService {
-  getManga() {
+  constructor(private http: HttpClient) { }
+
+  // 関数に形式と型を付けられる。httpでくる形式はObservable＋any型を指定
+  getManga():Observable<any> {
     console.log("MangaService経由してます");
-    return manga
+    return this.http.get('http://localhost:3001/manga-card-detail')
   }
 }
