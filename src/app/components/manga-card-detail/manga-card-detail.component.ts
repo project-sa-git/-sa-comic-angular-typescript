927f4810da29bc1474f7461cee0b02d017a23d73
mymac <trybook1901@gmail.com>
2022-11-06 18:48:58 +0900

    観測対象「observable」の放出・error確認

--- src/app/components/manga-card-detail/manga-card-detail.component.ts
+++ src/app/components/manga-card-detail/manga-card-detail.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MangaService } from '../service/manga.service';
-// import * as manga from '../../../assets/data/manga'
-// import { data } from '../../../assets/data/manga';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-manga-card-detail',
@@ -17,6 +16,33 @@ export class MangaCardDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.manga = this.mangaService.getManga()
+
+    // 観測対象「observable」を記述
+    const observable = new Observable((subscriber) => {
+      subscriber.next(1); // nextでデータを放出している
+      subscriber.next(2);
+      subscriber.error();
+      setTimeout(() => {  // 1s間待ってからnext4を放出
+        subscriber.next(4);
+        subscriber.complete();
+      }, 1000);
+    });
+
+    // debugger
+
+    console.log('just before subscribe');
+    observable.subscribe({
+      next(data) {
+        console.log('次のデータが出力 ' + data);
+      },
+      error(err) {
+        console.error('次のエラーが発生: ' + err);
+      },
+      complete() {
+        console.log('完了！');
+      },
+    });
+    console.log('subscribeから抜けました');
   }
 
 }
